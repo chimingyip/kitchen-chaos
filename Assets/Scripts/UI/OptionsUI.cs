21e97df69a8ae958cba4a452d0c170d8e14cdaa3
Chi-Ming Yip <cmyip99@gmail.com>
2023-02-23 13:21:06 +1100

    Options button and save state

--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -6,13 +6,18 @@ using TMPro;
 
 public class OptionsUI : MonoBehaviour
 {
+    public static OptionsUI Instance { get; private set; }
+
     [SerializeField] private Button soundEffectsButton;
     [SerializeField] private Button musicButton;
+    [SerializeField] private Button closeButton;
     [SerializeField] private TextMeshProUGUI soundEffectsText;
     [SerializeField] private TextMeshProUGUI musicText;
 
     private void Awake()
     {
+        Instance = this;
+
         soundEffectsButton.onClick.AddListener(() =>
         {
             SoundManager.Instance.ChangeVolume();
@@ -24,16 +29,40 @@ public class OptionsUI : MonoBehaviour
             MusicManager.Instance.ChangeVolume();
             UpdateVisual();
         });
+
+        closeButton.onClick.AddListener(() =>
+        {
+            Hide();
+        });
     }
 
     private void Start()
     {
+        _GameManager.Instance.OnGameUnpaused += GameManager_OnGameUnpaused;
+
         UpdateVisual();
+
+        Hide();
+    }
+
+    private void GameManager_OnGameUnpaused(object sender, System.EventArgs e)
+    {
+        Hide();
     }
 
     private void UpdateVisual()
     {
         soundEffectsText.text = "Sound Effects: " + Mathf.Round(SoundManager.Instance.GetVolume() * 10f);
         musicText.text = "Music: " + Mathf.Round(MusicManager.Instance.GetVolume() * 10f);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    
+    public void Hide()
+    {
+        gameObject.SetActive(false);
     }
 }
