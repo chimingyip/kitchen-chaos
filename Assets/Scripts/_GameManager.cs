25e440a322a272a0a09ff11ad732422ccc781fa7
Chi-Ming Yip <cmyip99@gmail.com>
2023-03-10 22:02:48 +1100

    fixing up the build and setting up netcode

--- Assets/Scripts/_GameManager.cs
+++ Assets/Scripts/_GameManager.cs
@@ -20,9 +20,9 @@ public class _GameManager : MonoBehaviour
     }
 
     private State state;
-    private float countdownToStartTimer = 3f;
+    private float countdownToStartTimer = 1f;
     private float gamePlayingTimer;
-    private float gamePlayingTimerMax = 120f;
+    private float gamePlayingTimerMax = 300f;
     private bool isGamePaused = false;
 
 
@@ -37,6 +37,10 @@ public class _GameManager : MonoBehaviour
     {
         GameInput.Instance.OnPauseAction += GameInput_OnPauseAction;
         GameInput.Instance.OnInteractAction += GameInput_OnInteractAction;
+
+        // DEBUG TRIGGER GAME START AUTOMATICALLY
+        state = State.CountdownToStart;
+        OnStateChanged?.Invoke(this, EventArgs.Empty);
     }
 
     private void GameInput_OnInteractAction(object sender, EventArgs e)
